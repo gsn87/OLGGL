9adb7a5c159e29028d19137dacb93acd5f8ee417
gsn87 <guillaume.simonin@gmail.com>
2015-03-25 23:01:22 +0100

    Update Plotter.ino

--- Plotter.ino
+++ Plotter.ino
@@ -1,4 +1,6 @@
+#include <Time.h>
 #include <Servo.h>
+
 
 //define CALIBRATION
 
@@ -13,17 +15,29 @@
 
 #define SERVOLEFTNULL 1660
 #define SERVORIGHTNULL 1070
+
+#define LIFT0 1850 // on drawing surface
+#define LIFT1 1720  // between numbers
+#define LIFT2 1480  // going towards sweeper
+
+#define LIFTSPEED 1300
  
 Servo ServoL;
 Servo ServoR;
+Servo ServoLift;
+
 int timeL = 1660;
 int timeR = 1100;
+
+int servoLift = 1500;
 
 int SERVOFAKTOR_LEFT = 650;
 int SERVOFAKTOR_RIGHT = 650;
 
 volatile double lastX = 78;
 volatile double lastY = 58;
+
+int last_min = 0;
  
 void setup()
 {
@@ -32,8 +46,15 @@ void setup()
     
     ServoL.attach(2);
     ServoR.attach(3);
+    ServoLift.attach(4);
+    lift(2);
+    setTime(21,47,0,0,0,0);
     
-    drawTo(78,58);
+    drawTo(143,67);
+    delay(2000);
+    lift(1);
+    delay(1000);
+    drawTo(80,67);
 
     //ServoL.writeMicroseconds(timeL);
     //ServoR.writeMicroseconds(timeR);
@@ -45,12 +66,14 @@ void loop()
   #ifdef CALIBRATION
 
     // Servohorns will have 90° between movements, parallel to x and y axis
-    delay(500);
-    drawTo(-12, 76);
-    delay(500);
-    drawTo(142, 76);
-    delay(500);
-    //lift(0);
+    delay(5000);
+    lift(2);
+    //drawTo(-12, 76);
+    delay(5000);
+    lift(1);
+    //drawTo(142, 76);
+    delay(5000);
+    lift(0);
 
   #else 
     //des données sur la liaison série ? (lorsque l'on appuie sur 'a' ou 'd')
@@ -65,13 +88,275 @@ void loop()
             drawTo(lastX, lastY+5); }
         else if(command == 'u') {
             drawTo(lastX, lastY-5); }
-        
+        else if(command == '0') {
+            lift(0); }
+        else if(command == '1') {
+            lift(1); }        
+        else if(command == '2'){
+            lift(2); }
+            
         //et on fait un retour sur la console pour savoir où on est rendu
         Serial.println(lastX);
         Serial.println(lastY);        
+    } 
+    
+/*    int i = 0;
+  if (last_min != minute()) {
+
+    if (!ServoLift.attached()) ServoLift.attach(4);
+    if (!ServoL.attached()) ServoL.attach(2);
+    if (!ServoR.attached()) ServoR.attach(3);
+
+    lift(0);
+
+    hour();
+    while ((i+1)*10 <= hour())
+    {
+      i++;
     }
+
+    number(3, 3, 111, 1);
+    number(5, 25, i, 0.9);
+    number(19, 25, (hour()-i*10), 0.9);
+    number(28, 25, 11, 0.9);
+
+    i=0;
+    while ((i+1)*10 <= minute())
+    {
+      i++;
+    }
+    number(34, 25, i, 0.9);
+    number(48, 25, (minute()-i*10), 0.9);
+    lift(2);
+    drawTo(74.2, 47.5);
+    lift(1);
+    last_min = minute();
+
+    ServoL.detach();
+    ServoR.detach();
+    ServoLift.detach();
+  } */
   #endif
 }
+
+// Writing numeral with bx by being the bottom left originpoint. Scale 1 equals a 20 mm high font.
+// The structure follows this principle: move to first startpoint of the numeral, lift down, draw numeral, lift up
+void number(float bx, float by, int num, float scale) {
+
+  switch (num) {
+
+  case 0:
+    drawTo(bx + 12 * scale, by + 6 * scale);
+    lift(0);
+    bogenGZS(bx + 7 * scale, by + 10 * scale, 10 * scale, -0.8, 6.7, 0.5);
+    lift(1);
+    break;
+  case 1:
+
+    drawTo(bx + 3 * scale, by + 15 * scale);
+    lift(0);
+    drawTo(bx + 10 * scale, by + 20 * scale);
+    drawTo(bx + 10 * scale, by + 0 * scale);
+    lift(1);
+    break;
+  case 2:
+    drawTo(bx + 2 * scale, by + 12 * scale);
+    lift(0);
+    bogenUZS(bx + 8 * scale, by + 14 * scale, 6 * scale, 3, -0.8, 1);
+    drawTo(bx + 1 * scale, by + 0 * scale);
+    drawTo(bx + 12 * scale, by + 0 * scale);
+    lift(1);
+    break;
+  case 3:
+    drawTo(bx + 2 * scale, by + 17 * scale);
+    lift(0);
+    bogenUZS(bx + 5 * scale, by + 15 * scale, 5 * scale, 3, -2, 1);
+    bogenUZS(bx + 5 * scale, by + 5 * scale, 5 * scale, 1.57, -3, 1);
+    lift(1);
+    break;
+  case 4:
+    drawTo(bx + 10 * scale, by + 0 * scale);
+    lift(0);
+    drawTo(bx + 10 * scale, by + 20 * scale);
+    drawTo(bx + 2 * scale, by + 6 * scale);
+    drawTo(bx + 12 * scale, by + 6 * scale);
+    lift(1);
+    break;
+  case 5:
+    drawTo(bx + 2 * scale, by + 5 * scale);
+    lift(0);
+    bogenGZS(bx + 5 * scale, by + 6 * scale, 6 * scale, -2.5, 2, 1);
+    drawTo(bx + 5 * scale, by + 20 * scale);
+    drawTo(bx + 12 * scale, by + 20 * scale);
+    lift(1);
+    break;
+  case 6:
+    drawTo(bx + 2 * scale, by + 10 * scale);
+    lift(0);
+    bogenUZS(bx + 7 * scale, by + 6 * scale, 6 * scale, 2, -4.4, 1);
+    drawTo(bx + 11 * scale, by + 20 * scale);
+    lift(1);
+    break;
+  case 7:
+    drawTo(bx + 2 * scale, by + 20 * scale);
+    lift(0);
+    drawTo(bx + 12 * scale, by + 20 * scale);
+    drawTo(bx + 2 * scale, by + 0);
+    lift(1);
+    break;
+  case 8:
+    drawTo(bx + 5 * scale, by + 10 * scale);
+    lift(0);
+    bogenUZS(bx + 5 * scale, by + 15 * scale, 5 * scale, 4.7, -1.6, 1);
+    bogenGZS(bx + 5 * scale, by + 5 * scale, 5 * scale, -4.7, 2, 1);
+    lift(1);
+    break;
+
+  case 9:
+    drawTo(bx + 9 * scale, by + 11 * scale);
+    lift(0);
+    bogenUZS(bx + 7 * scale, by + 15 * scale, 5 * scale, 4, -0.5, 1);
+    drawTo(bx + 5 * scale, by + 0);
+    lift(1);
+    break;
+
+  case 111:
+
+    lift(0);
+    drawTo(70, 46);
+    drawTo(65, 43);
+
+    drawTo(65, 49);
+    drawTo(5, 49);
+    drawTo(5, 45);
+    drawTo(65, 45);
+    drawTo(65, 40);
+
+    drawTo(5, 40);
+    drawTo(5, 35);
+    drawTo(65, 35);
+    drawTo(65, 30);
+
+    drawTo(5, 30);
+    drawTo(5, 25);
+    drawTo(65, 25);
+    drawTo(65, 20);
+
+    drawTo(5, 20);
+    drawTo(60, 44);
+
+    drawTo(75.2, 47);
+    lift(2);
+
+    break;
+
+  case 11:
+    drawTo(bx + 5 * scale, by + 15 * scale);
+    lift(0);
+    bogenGZS(bx + 5 * scale, by + 15 * scale, 0.1 * scale, 1, -1, 1);
+    lift(1);
+    drawTo(bx + 5 * scale, by + 5 * scale);
+    lift(0);
+    bogenGZS(bx + 5 * scale, by + 5 * scale, 0.1 * scale, 1, -1, 1);
+    lift(1);
+    break;
+
+  }
+}
+
+
+void lift(char lift) {
+  switch (lift) {
+    // room to optimize  !
+
+  case 0: //850
+
+      if (servoLift >= LIFT0) {
+      while (servoLift >= LIFT0) 
+      {
+        servoLift--;
+        ServoLift.writeMicroseconds(servoLift);				
+        delayMicroseconds(LIFTSPEED);
+      }
+    } 
+    else {
+      while (servoLift <= LIFT0) {
+        servoLift++;
+        ServoLift.writeMicroseconds(servoLift);
+        delayMicroseconds(LIFTSPEED);
+
+      }
+
+    }
+
+    break;
+
+  case 1: //150
+
+    if (servoLift >= LIFT1) {
+      while (servoLift >= LIFT1) {
+        servoLift--;
+        ServoLift.writeMicroseconds(servoLift);
+        delayMicroseconds(LIFTSPEED);
+
+      }
+    } 
+    else {
+      while (servoLift <= LIFT1) {
+        servoLift++;
+        ServoLift.writeMicroseconds(servoLift);
+        delayMicroseconds(LIFTSPEED);
+      }
+
+    }
+
+    break;
+
+  case 2:
+
+    if (servoLift >= LIFT2) {
+      while (servoLift >= LIFT2) {
+        servoLift--;
+        ServoLift.writeMicroseconds(servoLift);
+        delayMicroseconds(LIFTSPEED);
+      }
+    } 
+    else {
+      while (servoLift <= LIFT2) {
+        servoLift++;
+        ServoLift.writeMicroseconds(servoLift);				
+        delayMicroseconds(LIFTSPEED);
+      }
+    }
+    break;
+  }
+}
+
+void bogenUZS(float bx, float by, float radius, int start, int ende, float sqee) {
+  float inkr = -0.05;
+  float count = 0;
+
+  do {
+    drawTo(sqee * radius * cos(start + count) + bx,
+    radius * sin(start + count) + by);
+    count += inkr;
+  } 
+  while ((start + count) > ende);
+
+}
+
+void bogenGZS(float bx, float by, float radius, int start, int ende, float sqee) {
+  float inkr = 0.05;
+  float count = 0;
+
+  do {
+    drawTo(sqee * radius * cos(start + count) + bx,
+    radius * sin(start + count) + by);
+    count += inkr;
+  } 
+  while ((start + count) <= ende);
+}
+
 
 void drawTo(double pX, double pY) {
   double dx, dy, c;
@@ -81,7 +366,7 @@ void drawTo(double pX, double pY) {
   dx = pX - lastX;
   dy = pY - lastY;
   //path lenght in mm, times 4 equals 4 steps per mm
-  c = floor( 2*sqrt(dx * dx + dy * dy));
+  c = floor( 4*sqrt(dx * dx + dy * dy));
 
   if (c < 1) c = 1;
 
